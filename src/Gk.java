0c19959971368c185116dacfb16fefc891b06a83
AqAshiq <ashiq.ar7@gmail.com>
2023-01-02 10:24:17 -0800

    task

---
+++ src/Gk.java
@@ -0,0 +1,19 @@
+
+public class Gk {
+
+	public void m1() {
+	    System.out.println("aaaaaaaaa");
+		}
+
+		public void m2() {
+		    System.out.println("bbbbbbbbb");
+			}
+
+		
+		public void m3() {
+		    System.out.println("cccccccc");
+			}
+
+	}
+
+
